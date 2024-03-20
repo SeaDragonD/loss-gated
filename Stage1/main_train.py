92e441461ecdb747b2976e9b6cede2f2a537697c
lishouyang <lishouyang27@163.com>
2024-03-20 15:49:42 +0800

    [v2.0.2] add val_path

--- Stage1/main_train.py
+++ Stage1/main_train.py
@@ -15,12 +15,13 @@ parser.add_argument('--test_interval',     type=int,   default=1,            hel
 parser.add_argument('--max_epoch',         type=int,   default=80,           help='Maximum number of epochs')
 parser.add_argument('--lr',                type=float, default=0.001,        help='Learning rate')
 parser.add_argument("--lr_decay",          type=float, default=0.95,         help='Learning rate decay every [test_interval] epochs')
-parser.add_argument('--initial_model',     type=str,   default="exp/exp4/model/model000000041.model",           help='Initial model path')
+parser.add_argument('--initial_model',     type=str,   default="exp/exp4/model/model000000035.model",           help='Initial model path')
 parser.add_argument('--save_path',         type=str,   default="",           help='Path for model and scores.txt')
 parser.add_argument('--train_list',        type=str,   default="",           help='Path for Vox2 list, https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/train_list.txt')
 parser.add_argument('--test_list',          type=str,   default="",           help='Path for Vox_O list, https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/veri_test2.txt')
 parser.add_argument('--train_path',        type=str,   default="",           help='Path to the Vox2 set')
 parser.add_argument('--test_path',          type=str,   default="",           help='Path to the Vox_O set')
+parser.add_argument('--val_path',          type=str,   default="",           help='Path to the Vox_O set')
 parser.add_argument('--musan_path',        type=str,   default="",           help='Path to the musan set')
 parser.add_argument('--eval',              dest='eval', action='store_true', help='Do evaluation only')
 parser.add_argument('--label_num',         type=int,   default=4,            help='Number of label')
@@ -43,21 +44,25 @@ if(args.initial_model != ""): # If initial_model is exist, system will train fro
     Trainer.load_network(args.initial_model)
 elif len(modelfiles) >= 1: # Otherwise, system will try to start from the saved model&epoch
     Trainer.load_network(modelfiles[-1])
-    print(modelfiles[-1])
-    exit()
     it = int(os.path.splitext(os.path.basename(modelfiles[-1]))[0][5:]) + 1
 
 if 'new' in args.test_list.split('/')[-1]:
     import dataLoader_v2
     test_dataset = dataLoader_v2.get_testloader(args)
+    args.test_path = args.val_path
+    val_dataset = dataLoader_v2.get_testloader(args)
 else:
     test_dataset = get_testloader(args)
+    args.test_path = args.val_path
+    val_dataset = get_testloader(args)
 
 if args.eval == True: # Do evaluation only
     # EER, minDCF = Trainer.evaluate_network(**vars(args))
     # print('EER %2.4f, minDCF %.3f\n'%(EER, minDCF))
     avg_closs, Acc = Trainer.evaluate_network(loader=test_dataset)
-    print('avg_closs %2.4f, Acc %2.3f\n' % (avg_closs, Acc))
+    print('[TEST] avg_closs %2.4f, Acc %2.3f\n' % (avg_closs, Acc))
+    avg_closs, Acc = Trainer.evaluate_network(loader=val_dataset)
+    print('[VAL] avg_closs %2.4f, Acc %2.3f\n' % (avg_closs, Acc))
     quit()
 
 if 'new' in args.train_list.split('/')[-1]:
@@ -76,8 +81,15 @@ while it < args.max_epoch:
         avg_closs, Acc = Trainer.evaluate_network(loader=test_dataset)
         # print(time.strftime("%Y-%m-%d %H:%M:%S"), "LR %f, Acc %2.2f, LOSS %f, EER %2.4f, minDCF %.3f"%( lr, traineer, loss, EER, minDCF))
         # scorefile.write("Epoch %d, LR %f, Acc %2.2f, LOSS %f, EER %2.4f, minDCF %.3f\n"%(it, lr, traineer, loss, EER, minDCF))
-        print(time.strftime("%Y-%m-%d %H:%M:%S"),"LR %f, Acc %2.2f, LOSS %f, avg_closs %2.4f, Acc %2.3f" % (lr, traineer, loss, avg_closs, Acc))
-        scorefile.write("Epoch %d, LR %f, Acc %2.2f, LOSS %f, avg_closs %2.4f, Acc %2.3f\n"%(it, lr, traineer, loss, avg_closs, Acc))
+        print(time.strftime("%Y-%m-%d %H:%M:%S"),"[TEST] LR %f, Acc %2.2f, LOSS %f, avg_closs %2.4f, Acc %2.3f" % (lr, traineer, loss, avg_closs, Acc))
+        scorefile.write("[TEST] Epoch %d, LR %f, Acc %2.2f, LOSS %f, avg_closs %2.4f, Acc %2.3f\n"%(it, lr, traineer, loss, avg_closs, Acc))
+        scorefile.flush()
+
+        avg_closs, Acc = Trainer.evaluate_network(loader=val_dataset)
+        print(time.strftime("%Y-%m-%d %H:%M:%S"),
+              "[VAL] LR %f, Acc %2.2f, LOSS %f, avg_closs %2.4f, Acc %2.3f" % (lr, traineer, loss, avg_closs, Acc))
+        scorefile.write("[VAL] Epoch %d, LR %f, Acc %2.2f, LOSS %f, avg_closs %2.4f, Acc %2.3f\n" % (
+        it, lr, traineer, loss, avg_closs, Acc))
         scorefile.flush()
     # Otherwise, recored the training loss and acc
     else:
--- Stage1/run.sh
+++ Stage1/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python main_train.py \
---save_path exp/exp4 \
+--save_path exp/exp5 \
 --max_epoch 50 \
 --batch_size 64 \
 --lr 0.001 \
@@ -9,7 +9,8 @@ python main_train.py \
 --train_list /home/ai03/lsy/dataset/deepship/train_cut_new.txt \
 --test_list /home/ai03/lsy/dataset/deepship/test_cut_new.txt \
 --train_path /home/ai03/lsy/dataset/deepship/train \
---test_path /home/ai03/lsy/dataset/deepship/snr_experiment/test_soundscape_p15 \
+--test_path /home/ai03/lsy/dataset/deepship/test \
+--val_path /home/ai03/lsy/dataset/deepship/snr_experiment/test_soundscape_p15 \
 --musan_path /data08/Others/musan_split \
 --test_interval 1 \
 --eval
